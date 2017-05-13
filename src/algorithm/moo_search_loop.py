0a8f5b5c31ef3dd90a2a4a42680983a13f4c0778
Luiz Otavio V. B. Oliveira <luiz.vbo@gmail.com>
2017-05-13 01:02:53 +0100

    Fixed the function meps_weight_initialisation
    The function meps_weight_initialisation was not generating weights such
    that the sum of the weights was 1. Now, we ensure the sum is alwasy 1.

--- src/algorithm/moo_search_loop.py
+++ src/algorithm/moo_search_loop.py
@@ -32,28 +32,39 @@ def unpas_weight_initialisation(individuals):
 
 
 def meps_weight_initialisation(individuals):
+    # Number of objectives defined by the problem
     n_objectives = len(individuals[0].fitness)
+    # Number of divisions in the grid (by objective)
     length = params['WEIPS_LENGTH']
-    n_rows = length**n_objectives
     coordinates = numpy.linspace(0, 1, length)
-    coord_index = [0] * n_objectives
+    current_index = [0] * (n_objectives - 1)
+    current_index.append(length - 1)
+    max_index = [length - 1] * (n_objectives - 1)
     weight_matrix = []
 
-    for i in range(n_rows):
+    while current_index[0] <= max_index[0]:
         tmp_weigh = []
-        # Iterate through the coordinates
-        for j in range(n_objectives):
-            tmp_weigh.append(coordinates[coord_index[j]])
+        # Concatenate the weights and append to the matrix
+        for i in range(n_objectives):
+            tmp_weigh.append(coordinates[current_index[i]])
         weight_matrix.append(tmp_weigh)
-        # Increment the index of the last coordinate
-        coord_index[n_objectives-1] += 1
+        # Increment the last but one index
+        current_index[n_objectives-2] += 1
         # Check if we need to adjust the index of other coordinates
-        for j in range(n_objectives-1,0,-1):
-            if coord_index[j] >= len(coordinates):
-                coord_index[j] = 0
-                coord_index[j-1] += 1
+        for i in range(n_objectives - 2, 0, -1):
+            if current_index[i] > max_index[i]:
+                current_index[i] = 0
+                current_index[i-1] += 1
+                if current_index[i-1] <= max_index[i-1]:
+                    summation = 0
+                    for j in range(i):
+                        summation += current_index[j]
+                    for j in range(i, n_objectives - 1):
+                        max_index[j] = length - summation - 1
             else:
                 break
+        # The index of the last coordinate is simetric to the last but one
+        current_index[n_objectives - 1] = max_index[n_objectives - 2] - current_index[n_objectives - 2]
     return weight_matrix
 
 
