31a627e560de911f25674fe92042f40cd895061a
Luiz Otavio V. B. Oliveira <luiz.vbo@gmail.com>
2017-05-19 13:42:30 +0100

    Fitness function for regression problems with bloat control
    The bloat control is performed by defining a two-dimensional fitness
    function composed by the error and the size of the individual

---
+++ src/fitness/regression_bloat_control.py
@@ -0,0 +1,34 @@
+from math import isnan
+
+from fitness.regression import regression
+from . import supervised_learning
+
+from algorithm.parameters import params
+from utilities.fitness.error_metric import rmse
+
+
+class regression_bloat_control(regression):
+    """
+    Fitness function for regression using a multiobjective approach
+    to control bloat. 
+    We slightly specialise the function for supervised_learning.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self, ind, dist="training"):
+        objective_1 = super().__call__(ind, dist)
+        objective_1 = float(objective_1)
+        objective_2 = ind.nodes
+        return [objective_1, objective_2]
+
+    @staticmethod
+    def value(fitness_vector, objective_index):
+        if not isinstance(fitness_vector, list):
+            return float("inf")
+        return fitness_vector[objective_index]
+
+    @staticmethod
+    def num_objectives():
+        return 2
