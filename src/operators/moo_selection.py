0fb3bbec6f3a747a38e900a5069d8451433ef73f
Luiz Otavio V. B. Oliveira <luiz.vbo@gmail.com>
2017-06-12 14:34:50 -0300

    Fixed a small bug (we want the aggregated sum)

--- src/operators/moo_selection.py
+++ src/operators/moo_selection.py
@@ -354,7 +354,7 @@ def weips_comparison_operator(individual, other_individual, population_iqr, weig
     else:
         weights = weight_matrix[randint(0, len(weight_matrix) - 1)]
     # Normalize the weights according to the IQR of the objectives
-    weights = sum([a / b for a, b in zip(weights, population_iqr)])
+    weights = [a / b for a, b in zip(weights, population_iqr)]
     # Compute the fitness induced by the weights for each individual
     individual_f = sum([a * b for a, b in zip(individual.fitness, weights)])
     other_individual_f = sum([a * b for a, b in zip(other_individual.fitness, weights)])
